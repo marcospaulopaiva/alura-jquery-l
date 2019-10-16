2a760169018dd940104b0afff8beaada04904b42
Marcos Paulo O.P <marcos.poo@gmail.com>
2019-10-15 22:40:51 -0300

    Escutando eventos com jQuery - final da aula 2

--- js/main.js
+++ js/main.js
@@ -4,4 +4,14 @@ var tamanhaFrase = $("#tamanho-frase");
 tamanhaFrase.text(numPalavras);
 
 
-console.log(numPalavras);    
+var campo = $(".campo-digitacao");
+
+campo.on("input",function(){
+    var conteudo = campo.val();
+    
+    var qtdPalavras = conteudo.split(/\S+/).length -1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
